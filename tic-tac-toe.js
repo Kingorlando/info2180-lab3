1ea044c7ff0470f0f98b6339ecdcdb0c7bf99570
Orlando Williams <Kingorlando123@yahoo.com>
2020-10-11 16:58:14 -0500

    Updated js and css\\ files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,27 @@
 window.onload=function(){
+   let xIsNext=true;
+   scard=[];
    let squares=document.querySelectorAll('#board div');
-   var i;
-   for(i=0;i<squares.length;i++){
+   for(let i=0;i<squares.length;i++){
    squares[i].classList.add('square');
+   squares[i].addEventListener('click',function(){
+     console.log('Square ' + i + ' clicked');
+     if (xIsNext){
+       squares[i].classList.add('X');
+       let playerx=squares[i].innerHTML='X';
+       xIsNext=!xIsNext;
+       scard+=i
+       console.log(scard);
+       scard+=',';
+     }else{
+       squares[i].classList.add('O');
+       let playero=squares[i].innerHTML='O';
+       xIsNext=!xIsNext;
+       scard+=i
+       console.log(scard);
+       scard+=',';
+     }
+
+   });
    }
 }
