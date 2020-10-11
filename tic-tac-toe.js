72a3ff6fc3e7de2d59130a801128fd5240f91e9e
Orlando Williams <Kingorlando123@yahoo.com>
2020-10-11 16:00:55 -0500

    Added initial starter files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,29 +1,7 @@
-
-const btnDiv=document.querySelector('.btn');
-const squareDivs=document.querySelectorAll('.square');
-
-let gameisLive=true;
-let xIsNext=true;
-
-
-const entry=(e)=>{
-   const location=e.target.classList[1];
-   
-   if (e.target.classList[2]=='x' || e.target.classList[2]=='o'){
-     return;
+window.onload=function(){
+   let squares=document.querySelectorAll('#board div');
+   var i;
+   for(i=0;i<squares.length;i++){
+   squares[i].classList.add('square');
    }
-   
-   if (xIsNext){
-   e.target.classList.add('x');
-   xIsNext=!xIsNext;
-   }else{
-   e.target.classList.add('o');
-   xIsNext=!xIsNext;
-   }
-};
-
-
-
-for (const squareDiv of squareDivs){
-  squareDiv.addEventListener('click',entry)
 }
