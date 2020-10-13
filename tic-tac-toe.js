81f20d0a7ce0688efcdf14b3f2afa7be5d4dc1b6
Orlando Williams <Kingorlando123@yahoo.com>
2020-10-13 00:02:13 -0500

    Updated js and css files 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,11 +6,17 @@ window.onload=function(){
    for(let i=0;i<squares.length;i++){
    squares[i].classList.add('square');
 
+   squares[i].addEventListener('mouseover',function(){
+       squares[i].classList.add('hover');
+
+
    squares[i].addEventListener('click',function(){
-     console.log('Square ' + i + ' clicked');
+     console.log('Square ' + i + ' clicked');    
+     
       if (xIsNext){
        squares[i].classList.add('X');
        let playerx=squares[i].innerHTML='X';
+       
        xIsNext=!xIsNext;
        scard+=i
        console.log(scard);
@@ -18,47 +24,58 @@ window.onload=function(){
       }else{
        squares[i].classList.add('O');
        let playero=squares[i].innerHTML='O';
+       
        xIsNext=!xIsNext;
        scard+=i
        console.log(scard);
        scard+=',';
       }
-      if (squares[0].innerHTML==squares[1].innerHTML && squares[1].innerHTML==squares[2].innerHTML){
-        console.log('x wins');
+      
+      if (squares[0].innerHTML=='X' && squares[0].innerHTML==squares[1].innerHTML && squares[1].innerHTML==squares[2].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[0].innerHTML=='O' && squares[0].innerHTML==squares[1].innerHTML && squares[1].innerHTML==squares[2].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+      }else if (squares[3].innerHTML=='X' && squares[3].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[5].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[3].innerHTML=='O' && squares[3].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[5].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+      }else if (squares[6].innerHTML=='X' && squares[6].innerHTML==squares[7].innerHTML && squares[7].innerHTML==squares[8].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[6].innerHTML=='O' && squares[6].innerHTML==squares[7].innerHTML && squares[7].innerHTML==squares[8].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+      }else if (squares[0].innerHTML=='X' && squares[0].innerHTML==squares[3].innerHTML && squares[3].innerHTML==squares[6].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[0].innerHTML=='O' && squares[0].innerHTML==squares[3].innerHTML && squares[3].innerHTML==squares[6].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+      }else if (squares[1].innerHTML=='X' && squares[1].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[7].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[1].innerHTML=='O' && squares[1].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[7].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+      }else if (squares[2].innerHTML=='X' && squares[2].innerHTML==squares[5].innerHTML && squares[5].innerHTML==squares[8].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[2].innerHTML=='O' && squares[2].innerHTML==squares[5].innerHTML && squares[5].innerHTML==squares[8].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+      }else if (squares[0].innerHTML=='X' && squares[0].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[8].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[0].innerHTML=='O' && squares[0].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[8].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+      }else if (squares[2].innerHTML=='X' && squares[2].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[6].innerHTML ){
+        document.getElementById("status").innerHTML="x wins";
+      }else if (squares[2].innerHTML=='O' && squares[2].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[6].innerHTML){
+        document.getElementById("status").innerHTML="o wins";
+
       }
-     function Board(){
-       this.positions=Array.from(document.querySelectorAll('.square'));
-       
-         this.checkwinner=function(){
-         let winner=false;
-          const wcombo=[
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-          ];
-          const positions=this.positions;
-          wcombo.forEach((combo)=>{
-            const pos0innertext=positions[wcombo[0]].innerText;
-            const pos1innertext=positions[wcombo[1]].innerText;
-            const pos2innertext=positions[wcombo[2]].innerText;
-            const isWcombo=pos0innertext!=='' && pos0innertext==pos1innertext && pos1innertext==pos2innertext;
-            
-            if (isWcombo){
-              winner=true;
-              combo.forEach((index)=>{
-                positions[index].className+='winner';
-              })
-            }
-          });
-          return winner;
-       }
-     }
-     
+
+
+
+
+
+
+
+
+      
+        }); 
    });
+  
    }
 }
